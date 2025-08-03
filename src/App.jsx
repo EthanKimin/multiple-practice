8a1b65bba8b65433c1bb9651694c3fc4a57ec6ed
Ethan Kim <rlagmlruadle@icloud.com>
2025-08-03 21:27:11 +0300

    make test

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import { useState } from "react";
 import "./App.css";
 import Timer from "./Timer";
+import Test from "./Test";
 
 const App = () => {
   const [isRunning, setIsRunning] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [allCorrect, setAllCorrect] = useState(false);
 
   return (
     <>
-      <div className="">
-        <Timer isRunning={isRunning} />
-        <input
-          type="button"
-          value="시작"
+      <h1>곱셈 연습</h1>
+      <div className="body">
+        <button
           className="strtBtn"
-          onClick={() => setIsRunning(true)}
-        />
+          onClick={() => {
+            setIsRunning(true);
+            setVisible(true);
+          }}
+        >
+          시작
+        </button>
+        <Timer isRunning={isRunning} />
+        <Test visible={visible} maxNum={9} onAllCorrectChange={setAllCorrect} />
+        {allCorrect && (
+          <button className="submitBtn" onClick={() => {}}>
+            제출
+          </button>
+        )}
       </div>
     </>
   );
