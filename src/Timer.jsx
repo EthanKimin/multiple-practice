181483e062f90ec9ce8f52e8ce9d66ade2e9b12e
Ethan Kim <rlagmlruadle@icloud.com>
2025-08-03 20:21:32 +0300

    make start btn and fix timer

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,17 +1,23 @@
 import { useEffect, useRef, useState } from "react";
 
-const Timer = (isRunning) => {
+// eslint-disable-next-line react/prop-types
+const Timer = ({ isRunning }) => {
   const [second, setSecond] = useState(0);
-
   const intervalRef = useRef(null);
-  useEffect(() => {
-    intervalRef.current = setInterval(() => {
-      setSecond((prev) => prev + 1);
-    }, 1000);
 
-    // 컴포넌트가 언마운트될 때 interval 정리
+  useEffect(() => {
+    if (isRunning) {
+      intervalRef.current = setInterval(() => {
+        setSecond((prev) => prev + 1);
+      }, 1000);
+    } else {
+      // 정지 시 interval 제거
+      clearInterval(intervalRef.current);
+    }
+
+    // 컴포넌트가 언마운트되거나 isRunning 변경될 때 정리
     return () => clearInterval(intervalRef.current);
-  }, []);
+  }, [isRunning]); // ✅ isRunning을 의존성 배열에 추가
 
   return (
     <div className="timeContainer">
