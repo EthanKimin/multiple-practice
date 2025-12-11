53fea4b00ee8eb56ab41b6868088d3095555ddd3
Ethan Kim <rlagmlruadle@icloud.com>
2025-12-11 22:12:03 +0300

    fix style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css";
 const Home = () => {
   return (
     <>
-      <section className="welcome page">
+      <div className="welcome page">
         <div className="welcome__container1">
           <div className="welcome__container1__element1">Mathmatics</div>
           <div className="welcome__container1__element2">is</div>
@@ -13,12 +13,12 @@ const Home = () => {
           <div className="welcome__container2__element4">&</div>
           <div className="welcome__container2__element5">Progress</div>
         </div>
-      </section>
-      <section className="page explain">
+      </div>
+      <div className="page explain">
         <div className="explain_text">
           Math Trail은 기본부터 심화까지 철저히 연습하게 도와줍니다.
         </div>
-      </section>
+      </div>
     </>
   );
 };
