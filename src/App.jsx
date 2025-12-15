32771fb79f1b7817b7e8ec04599400ce41db6439
Ethan Kim <rlagmlruadle@icloud.com>
2025-12-15 10:48:02 +0300

    fix page loading bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import "./App.css";
 import Header from "./layouts/Header.jsx";
 import Footer from "./layouts/Footer.jsx";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Basic from "./pages/Basic";
 import Decimal from "./pages/Decimal";
 import Fraction from "./pages/Fraction";
 import Geometry from "./pages/Geometry";
 import Statistics from "./pages/Statistics";
+import { useEffect, useRef } from "react";
 
 const App = () => {
+  const mainRef = useRef(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (mainRef.current) {
+      mainRef.current.scrollTo(0, 0);
+    }
+  }, [location.pathname]);
+
   return (
     <>
       <Header />
       {/* 라우트에 따라 내용이 변경되는 영역 */}
-      <main className="main-content-snapping-area">
+      <main ref={mainRef} className="main-content-snapping-area">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/basic" element={<Basic />} />
